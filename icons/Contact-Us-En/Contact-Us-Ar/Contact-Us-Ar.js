2a5c82f21a62531a19909f21142b038a37281f80
Ahmed3enba <3enabe22@gmail.com>
2026-07-17 17:06:05 +0300

    Add files via upload

---
+++ icons/Contact-Us-En/Contact-Us-Ar/Contact-Us-Ar.js
@@ -0,0 +1,34 @@
+let home=document.getElementById('home');
+let about=document.getElementById('about');
+let solutions=document.getElementById('solutions');
+let academy=document.getElementById('academy');
+let contact=document.getElementById('contact');
+let homeLi=document.getElementById('homeLi');
+let aboutLi=document.getElementById('aboutLi');
+let solutionsLi=document.getElementById('solutionsLi');
+let academyLi=document.getElementById('academyLi');
+let contactLi=document.getElementById('contactLi');
+let button=document.getElementById('button');
+let btn=document.getElementById('btn');
+let btn1=document.getElementById('btn1');
+let a1=document.getElementById('a1');
+let over=document.getElementById('over');
+let creative2=document.getElementById('creative2');
+let creative3=document.getElementById('creative3');
+let creative4=document.getElementById('creative4');
+let overTop=document.getElementById('overTop');
+let overTop2=document.getElementById('overTop2');
+let overTop3=document.getElementById('overTop3');
+let overTop4=document.getElementById('overTop4');
+let test=document.getElementById('test')
+contactLi.style.cssText=`
+border-bottom: 2px solid #006992;
+`
+contact.style.cssText=`
+color: #006992;
+`
+solutions.onclick=function(){
+    test.classList.toggle('think');
+}
+solutions.style.cssText=`
+`
